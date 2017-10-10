501e2836974e2ea2f669bb226ae437b47431db66
Beaston4 <32681798+Beaston4@users.noreply.github.com>
2017-10-10 11:53:52 -0500

    Add files via upload

---
+++ Fetch.py
@@ -0,0 +1,42 @@
+#Fetch that
+
+import urllib.request
+#import enchant
+
+req = urllib.request.Request('http://www.voidspace.org.uk')
+with urllib.request.urlopen(req) as response:
+   the_page = response.read()
+page = str(the_page)
+page = page.split()
+
+
+cleanPage = []
+abc = 'abcdefghijklmnopqrstuvwxyz'
+ABC = abc.upper()
+letters = abc + ABC
+for e in page:
+    word = ''
+    for c in e:
+        if c in letters:
+            word += c
+    if word != '':
+        cleanPage.append(word)
+
+#print(cleanPage[1:100])
+'''
+d = enchant.Dict("en_US")
+
+cleanPage = []
+for i in page:
+    if d.check(page[i]):
+        cleanPage.append(page[i])
+
+print(cleanPage[1:100])
+'''
+
+val = input("What word would you like to find: ")
+x = 0
+for i in range(len(cleanPage)):
+    if cleanPage[i] == val:
+        x += 1
+print("The word", val, "was found", x, "times")
