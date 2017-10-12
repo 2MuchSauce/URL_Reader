41f0ee344758396e56cb8171fde36d2dfa8e91ee
billybimbob <32024142+billybimbob@users.noreply.github.com>
2017-10-12 12:14:52 -0500

    Update Fetch.py

--- Fetch.py
+++ Fetch.py
@@ -1,20 +1,27 @@
 from newspaper import Article
-url = input("URL: ")
-#url = 'http://www.cnn.com/2017/10/10/entertainment/donna-karan-harvey-weinstein/index.html'
 
-article = Article(url, language='en') # English
+def findWord():
+ while(True):
+  try:
+   url = input("URL: ")
+   article = Article(url, language='en') # English
+  except:
+   print("Your url is invalid")
+  #url = 'http://www.cnn.com/2017/10/10/entertainment/donna-karan-harvey-weinstein/index.html'
 
-article.download()
-article.parse()
+ article.download()
+ article.parse()
 
-print(article.text[:])
+ print(article.text[:])
 
-val = input("What word would you like to find: ")
- 
-x = 0
+ val = input("What word would you like to find: ")
 
-articleArray = article.text.split()
-for i in range(len(articleArray)):
-        if val.lower() in articleArray[i].lower():
-                x += 1
-print("The word", val, "was found", x, "times")
+ x = 0
+
+ articleArray = article.text.split()
+ for i in range(len(articleArray)):
+         if val.lower() in articleArray[i].lower():
+                 x += 1
+ print("The word", val, "was found", x, "times")
+
+findWord()
