6bdb53f6d93ca4bd85cbdaa52ec01b2473912209
bennyp3333 <bennyp3333@gmail.com>
2017-10-12 12:26:09 -0500

    Loop

--- Fetch.py
+++ Fetch.py
@@ -1,27 +1,32 @@
 from newspaper import Article
 
 def findWord():
- while(True):
-  try:
-   url = input("URL: ")
-   article = Article(url, language='en') # English
-  except:
-   print("Your url is invalid")
-  #url = 'http://www.cnn.com/2017/10/10/entertainment/donna-karan-harvey-weinstein/index.html'
+    while(True):
+        try:
+            url = input("URL: ")
+            article = Article(url, language='en') # English
+            article.download()
+            article.parse()
+            break
+        except:
+            print("Your url is invalid")
 
- article.download()
- article.parse()
+    #url = 'http://www.cnn.com/2017/10/10/entertainment/donna-karan-harvey-weinstein/index.html'
+    
+    print(article.text[:])  
+    while(True):
+        print("[Input 0 to Quit]")
+        val = input("What word would you like to find: ")
+        if(val == "0"):
+            print("Program has ended")
+            break
+        x = 0
 
- print(article.text[:])
-
- val = input("What word would you like to find: ")
-
- x = 0
-
- articleArray = article.text.split()
- for i in range(len(articleArray)):
-         if val.lower() in articleArray[i].lower():
-                 x += 1
- print("The word", val, "was found", x, "times")
+        articleArray = article.text.split()
+        for i in range(len(articleArray)):
+            if val.lower() in articleArray[i].lower():
+                x += 1
+        print("The word", val, "was found", x, "times")
+    
 
 findWord()
